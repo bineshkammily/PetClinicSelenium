73d4fc4e05573d956dea095223083a5c844815e6
bineshkammily <bineshkammily@gmail.com>
2017-09-17 18:09:13 +0530

    Update seleniumExec.bat

--- seleniumExec.bat
+++ seleniumExec.bat
@@ -1,6 +1,6 @@
-javac petclinic\selenium\AppTest.java
-
 set CLASSPATH=%CLASSPATH%;C:\Selenium\selenium-server-standalone-2.53.0.jar;C:\Selenium\testng-6.8.8.jar;.
+
+javac petclinic\selenium\AppTest.java
 
 java org.testng.TestNG testng.xml
 
