f5041479f7ef7d56de26855a7758806e60da3a8b
Kammily <binesh.kammily@accenture.com>
2017-09-17 17:08:07 +0530

    all selenium test files

---
+++ petclinic/selenium/AppTest.java
@@ -0,0 +1,32 @@
+package petclinic.selenium;
+
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.testng.Assert;
+import org.testng.annotations.AfterSuite;
+import org.testng.annotations.BeforeSuite;
+import org.testng.annotations.Test;
+
+public class AppTest {
+	WebDriver driver=null;
+	
+	@BeforeSuite(alwaysRun = true)
+	public void initialize(){
+		System.setProperty("webdriver.chrome.driver","Driver\\chromedriver.exe");
+		driver=new ChromeDriver();
+	}
+
+	@Test
+	public void testMethod() {
+		driver.get("http://18.194.36.139:8080/petclinic/");
+		System.out.println(driver.getTitle());
+		Assert.assertEquals("PetClinic :: a Spring Framework demonstration", driver.getTitle());
+	}
+	
+	@AfterSuite
+	public void clear(){
+		driver.close();
+	}
+	
+}
+
