8130b92287aab0e381771cc8392e3a6949c90574
bineshkammily <bineshkammily@gmail.com>
2017-09-17 18:07:48 +0530

    Update seleniumExec.bat

--- seleniumExec.bat
+++ seleniumExec.bat
@@ -1,5 +1,7 @@
 javac petclinic\selenium\AppTest.java
 
-java -cp C:\Selenium\selenium-server-standalone-2.53.0.jar;C:\Selenium\testng-6.8.8.jar;. org.testng.TestNG testng.xml
+set CLASSPATH=%CLASSPATH%;C:\Selenium\selenium-server-standalone-2.53.0.jar;C:\Selenium\testng-6.8.8.jar;.
+
+java org.testng.TestNG testng.xml
 
 echo "Selenium Execution completed successfully"
